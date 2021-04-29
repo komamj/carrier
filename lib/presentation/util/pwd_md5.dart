85b379d4fe7b649f31f6123a3697a118de89ce36
nicole.fu <nicole.fu@nidone.com.cn>
2021-04-29 17:35:17 +0800

    feat: [global] #N/A pwd md5

--- lib/presentation/util/pwd_md5.dart
+++ lib/presentation/util/pwd_md5.dart
@@ -1,6 +1,6 @@
 import 'package:crypto/crypto.dart';
 
-getPwd(data) {
+String getPwd(String data) {
   var a = data;
   var b = '\x8f\x70\x83\x8f';
   var c = '2eh1.iaqw7';
@@ -8,9 +8,9 @@ getPwd(data) {
   return s;
 }
 
-getPwdMd5Val(pwd) {
+String getPwdMd5Val(String pwd) {
   String str = getPwd(pwd);
   List<int> bytes = str.codeUnits; // data being hashed
   var digest = md5.convert(bytes);
-  return digest;
+  return digest.toString();
 }
