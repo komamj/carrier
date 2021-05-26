70785ee9fc2afe4901a8e50687670bc7815cf503
nicole.fu <nicole.fu@nidone.com.cn>
2021-05-26 15:37:26 +0800

    feat: [mine] #N/A  获取本地 getUser  方法重写

--- lib/presentation/mine/mine_view_model.dart
+++ lib/presentation/mine/mine_view_model.dart
@@ -7,15 +7,7 @@ class MineViewModel extends BaseViewModel {
   final MineRepository repository;
   String aliOssUrl =
       'https://my-oss-bucket-epressarrive0c7c7eb2-959e-47f7-83a5-91a2559bd416.oss-cn-beijing.aliyuncs.com';
-  String _avatar =
-      "https://pics7.baidu.com/feed/a8773912b31bb051fb37de05c78e64b24bede083.jpeg?token=f02d22e51399a01c6c239e6247cec44f";
-  String get avatar => _avatar;
-  String _name = "";
-  String get name => _name;
-  String _mobile = "";
-  String get mobile => _mobile;
-  String _companyName = "";
-  String get companyName => _companyName;
+
   MineViewModel({required this.repository});
 
   Future<User> getUser() async {
